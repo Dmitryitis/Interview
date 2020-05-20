ee8efd65c180889eb682839dbc332c44c7a38e18
Dmitryitis <61817851+Dmitryitis@users.noreply.github.com>
2020-05-20 15:20:16 +0300

    Add files via upload

---
+++ AboutMe.java
@@ -0,0 +1,9 @@
+package ru.kpfu.itis.group905.gerasimov.primer.SER;
+
+public class AboutMe {
+    public static void main(String[] args) {
+        System.out.println("My name is Dmitry Gerasimov. " +
+                "I from 11-905.\\n" +
+                "I like python.");
+    }
+}
---
+++ AboutMe.py
@@ -0,0 +1,4 @@
+about_me = "My name is Dmitry Gerasimov.\n"\
+                "I from 11-905.\n"\
+                "I like python."
+print(about_me)
